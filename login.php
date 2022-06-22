313a2eb4d8877b83550919f929ad58c447999d07
Ng Qi Teng <Cmdcool3@gmail.com>
2022-06-22 10:22:16 +0800

    Add Icon Image

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- <link rel="icon" type="image/x-icon" href="<?= '' // $config[ 'urls' ][ 'img' ] . "favicon.png"; ?>" /> -->
+    <link rel="icon" type="image/x-icon" sizes="32x32" href="<?= $config[ 'urls' ][ 'img' ] . "icon.ico"; ?>" />
     
     <title> System | Login </title>
 
--- templates/head.php
+++ templates/head.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!-- Icon Start -->
-<!-- <link rel="icon" type="image/x-icon" href="<?= '' // $config[ 'urls' ][ 'img' ] . "favicon.png"; ?>" /> -->
+<link rel="icon" type="image/x-icon" sizes="32x32" href="<?= $config[ 'urls' ][ 'img' ] . "icon.ico"; ?>" />
 <!-- Icon End -->
 
 <!-- Bootstrap 5 Start -->
