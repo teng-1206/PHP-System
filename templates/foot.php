e603ba9851b51053ed7a70fa965a22e10da5ea61
Ng Qi Teng <Cmdcool3@gmail.com>
2022-06-22 10:06:55 +0800

    Add Template Files

---
+++ templates/foot.php
@@ -0,0 +1,20 @@
+<!-- Bootstrap 5 Start -->
+<script src="<?= $config[ 'urls' ][ 'plugins' ] . "bootstrap/bootstrap.min.js"; ?>"></script>
+<script src="<?= $config[ 'urls' ][ 'plugins' ] . "bootstrap/bootstrap.bundle.min.js"; ?>"></script>
+<!-- Bootstrap 5 End -->
+
+<!-- Material Bootstrap 5 Start -->
+<!-- <script src="<?= '' // $config[ 'urls' ][ 'plugins' ] . "material-bootstrap/mdb.min.js"; ?>"></script> -->
+<!-- Material Bootstrap 5 End -->
+
+<!-- jQuery Start -->
+<script src="<?= $config[ 'urls' ][ 'plugins' ] . "jquery/jquery.min.js"; ?>"></script>
+<!-- jQuery End -->
+
+<!-- Sweet Alert 2 Start -->
+<script src="<?= $config[ 'urls' ][ 'plugins' ] . "sweetalert2/sweetalert2.all.min.js"; ?>"></script>
+<!-- Sweet Alert 2 End -->
+
+<!-- Font Awesome 5 Start -->
+<script src="<?= $config[ 'urls' ][ 'plugins' ] . "font-awesome/js/all.min.js"; ?>"></script>
+<!-- Font Awesome 5 End -->
---
+++ templates/head.php
@@ -0,0 +1,23 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Icon Start -->
+<!-- <link rel="icon" type="image/x-icon" href="<?= '' // $config[ 'urls' ][ 'img' ] . "favicon.png"; ?>" /> -->
+<!-- Icon End -->
+
+<!-- Bootstrap 5 Start -->
+<link rel="stylesheet" href="<?= $config[ 'urls' ][ 'plugins' ] . "bootstrap/bootstrap.min.css"; ?>" />
+<!-- Bootstrap 5 End -->
+
+<!-- Material Bootstrap 5 Start -->
+<!-- <link rel="stylesheet" href="<?= '' // $config[ 'urls' ][ 'plugins' ] . "material-bootstrap/mdb.min.css"; ?>" /> -->
+<!-- Material Bootstrap 5 End -->
+
+<!-- Sweet Alert 2 Start -->
+<link rel="stylesheet" href="<?= $config[ 'urls' ][ 'plugins' ] . "sweetalert2/sweetalert2.min.css"; ?>" />
+<!-- Sweet Alert 2 End -->
+
+<!-- Font Awesome 5 Start -->
+<link rel="stylesheet" href="<?= $config[ 'urls' ][ 'plugins' ] . "font-awesome/css/all.min.css"; ?>" />
+<!-- Font Awesome 5 End -->
