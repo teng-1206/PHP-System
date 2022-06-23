6583f79529f9c96be207a44f39cdae714063bc3c
Ng Qi Teng <Cmdcool3@gmail.com>
2022-06-23 22:31:21 +0800

    Create Dashboard Page

---
+++ dashboard.php
@@ -0,0 +1,27 @@
+<?php include_once( realpath( dirname( __FILE__ ) . "/config/config.php" ) ); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> System | Dashboard </title>
+    <!-- Global CSS Start -->
+    <?php include_once( TEMPLATES_PATH . 'head.php' ); ?>
+    <!-- Global CSS End -->
+
+    <!-- Custom CSS Start -->
+    <!-- Custom CSS End -->
+</head>
+<body>
+    <!-- Navbar Start -->
+    <?php include_once( TEMPLATES_PATH . 'navbar.php' ); ?>
+    <!-- Navbar End -->
+
+
+    <!-- Global JS Start -->
+    <?php include_once( TEMPLATES_PATH . 'foot.php' ); ?>
+    <!-- Global JS End -->
+
+    <!-- Custom JS Start -->
+    <!-- Custom JS End -->
+</body>
+</html>
