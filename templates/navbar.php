52350c1e52f677bb00448f896bdf74f189ca9a5a
Ng Qi Teng <Cmdcool3@gmail.com>
2022-06-23 22:31:29 +0800

    Create Navbar

---
+++ templates/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img class="" src="<?= $config[ 'urls' ][ 'img' ] . "icon-128x128.png"; ?>" height="32px" width="32px" alt="Icon">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0 ml-2">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Finance</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profile</a>
+                </li>
+            </ul>
+            <div class="d-flex">
+                <button class="btn btn-danger" type="button">
+                    <i class="fas fa-power-off text-white"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</nav>
