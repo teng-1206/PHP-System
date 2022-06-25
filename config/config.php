f3eaf3cf047946b2e1d0fba4400df7ff3d764bf6
Ng Qi Teng <Cmdcool3@gmail.com>
2022-06-26 01:52:15 +0800

    Update Config

--- config/config.php
+++ config/config.php
@@ -5,77 +5,70 @@
     $config = array(
         "db" => array(
             "db1" => array(
-                "dbname" => "database1",
-                "username" => "dbUser",
-                "password" => "pa$$",
-                "host" => "localhost"
+                "dbname"   => "system",
+                "username" => "root",
+                "password" => "",
+                "host"     => "localhost"
             ),
-            "db2" => array(
-                "dbname" => "database2",
-                "username" => "dbUser",
-                "password" => "pa$$",
-                "host" => "localhost"
-            )
+            // "db2" => array(
+            //     "dbname"   => "database2",
+            //     "username" => "dbUser",
+            //     "password" => "pa$$",
+            //     "host"     => "localhost"
+            // )
         ),
         "urls" => array(
-            "base" => "http://example.com"
-        ),
-        "localhost_urls" => array(
-            "base" => "https://localhost/FOLDER_NAME/",
-            "api" => "https://localhost/FOLDER_NAME/assets/api/",
-            "config" => "https://localhost/FOLDER_NAME/assets/config/",
-            "css" => "https://localhost/FOLDER_NAME/assets/css/",
-            "docs" => "https://localhost/FOLDER_NAME/assets/docs/",
-            "img" => "https://localhost/FOLDER_NAME/assets/img/",
-            "js" => "https://localhost/FOLDER_NAME/assets/js/",
-            "modules" => "https://localhost/FOLDER_NAME/assets/modules/",
-            "plugins" => "https://localhost/FOLDER_NAME/assets/plugins/",
-            "templates" => "https://localhost/FOLDER_NAME/assets/templates/",
-            "uploads" => "https://localhost/FOLDER_NAME/assets/uploads/",
+            "base"      => "http://localhost/php-system/",
+            "api"       => "http://localhost/php-system/api/",
+            "config"    => "http://localhost/php-system/config/",
+            "css"       => "http://localhost/php-system/css/",
+            "img"       => "http://localhost/php-system/img/",
+            "js"        => "http://localhost/php-system/js/",
+            "modules"   => "http://localhost/php-system/modules/",
+            "plugins"   => "http://localhost/php-system/plugins/",
+            "templates" => "http://localhost/php-system/templates/",
+            "uploads"   => "http://localhost/php-system/uploads/",
             
         ),
         "paths" => array(
             "resources" => "/path/to/assets",
             "images" => array(
                 "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
-                "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
+                "layout"  => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
             )
         )
     );
 
-    include_once("conn.php");
-          
+    include_once( 'conn.php' );
+
     defined("ASSETS_PATH")
-        or define("ASSETS_PATH", realpath(dirname(__FILE__) . '/..//'));
+        or define("ASSETS_PATH", realpath( dirname( __FILE__ ) . '/..//' ) );
         
     defined("API_PATH")
-        or define("API_PATH", ASSETS_PATH . '/api');
+        or define("API_PATH", ASSETS_PATH . '/api/');
 
     defined("CONFIG_PATH")
-        or define("CONFIG_PATH", ASSETS_PATH . '/config');
+        or define("CONFIG_PATH", ASSETS_PATH . '/config/');
 
     defined("CSS_PATH")
-        or define("CSS_PATH", ASSETS_PATH . '/css');
+        or define("CSS_PATH", ASSETS_PATH . '/css/');
 
-    defined("DOCS_PATH")
-        or define("DOCS_PATH", ASSETS_PATH . '/docs');
-        
     defined("IMG_PATH")
-        or define("IMG_PATH", ASSETS_PATH . '/img');
+        or define("IMG_PATH", ASSETS_PATH . '/img/');
 
     defined("JS_PATH")
-        or define("JS_PATH", ASSETS_PATH . '/js');
+        or define("JS_PATH", ASSETS_PATH . '/js/');
 
     defined("MODULES_PATH")
-        or define("MODULES_PATH", ASSETS_PATH . '/modules');
+        or define("MODULES_PATH", ASSETS_PATH . '/modules/');
 
     defined("PLUGINS_PATH")
-        or define("PLUGINS_PATH", ASSETS_PATH . '/plugins');   
+        or define("PLUGINS_PATH", ASSETS_PATH . '/plugins/');   
     
     defined("TEMPLATES_PATH")
-        or define("TEMPLATES_PATH", ASSETS_PATH . '/templates');
+        or define("TEMPLATES_PATH", ASSETS_PATH . '/templates/');
 
     defined("UPLOADS_PATH")
-        or define("UPLOADS_PATH", ASSETS_PATH . '/uploads');
-     
+        or define("UPLOADS_PATH", ASSETS_PATH . '/uploads/');
+
 ?>
