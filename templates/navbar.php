2b8ccb20b08e0e2761818a6559e4a32185f4ac92
Ng Qi Teng <Cmdcool3@gmail.com>
2022-06-26 01:32:09 +0800

    Update Navbar

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="dashboard.php">
             <img class="" src="<?= $config[ 'urls' ][ 'img' ] . "icon-128x128.png"; ?>" height="32px" width="32px" alt="Icon">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,19 +9,19 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0 ml-2">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                    <a class="nav-link active" aria-current="page" href="dashboard.php">Dashboard</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Finance</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Profile</a>
+                    <a class="nav-link" href="profile.php">Profile</a>
                 </li>
             </ul>
             <div class="d-flex">
-                <button class="btn btn-danger" type="button">
+                <a class="btn btn-danger" type="button">
                     <i class="fas fa-power-off text-white"></i>
-                </button>
+                </a>
             </div>
         </div>
     </div>
