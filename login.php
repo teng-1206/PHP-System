633bca8efac06ea1c909a084678c78fedb7beb18
Ng Qi Teng <Cmdcool3@gmail.com>
2022-06-27 22:29:45 +0800

    Add API Meta

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta id="api-url" name="api-url" content="<?= $config[ 'urls' ][ 'api' ] ?>" />
 
     <link rel="icon" type="image/x-icon" sizes="32x32" href="<?= $config[ 'urls' ][ 'img' ] . "icon.ico"; ?>" />
     
