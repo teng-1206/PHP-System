9e77a01d0049499e926776d77be063b88ca29dec
Ng Qi Teng <Cmdcool3@gmail.com>
2022-06-27 22:30:39 +0800

    Update Database Connection

--- config/conn.php
+++ config/conn.php
@@ -1,9 +1,15 @@
 <?php
 
-    $conn = new mysqli($config['db']['db1']['host'], $config['db']['db1']['username'], $config['db']['db1']['password'], $config['db']['db1']['dbname']);        
-    
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    try {
+        $conn = new PDO(
+            "mysql:host=" . $config[ 'db' ][ 'db1' ][ 'host' ] . ";dbname=" . $config[ 'db' ][ 'db1' ][ 'dbname' ],
+            $config[ 'db' ][ 'db1' ][ 'username' ],
+            $config[ 'db' ][ 'db1' ][ 'password' ]
+        );
+        $conn->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch( PDOException $ex ) {
+        die( "Connection failed: " . $ex->getMessage() );
     }
 
 ?>
