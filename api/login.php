8b3e08e9a7f4a749e5946aefce1c762ace140da3
Ng Qi Teng <Cmdcool3@gmail.com>
2022-06-27 22:31:49 +0800

    Completed Login Function

---
+++ api/login.php
@@ -0,0 +1,23 @@
+<?php
+
+    include_once( realpath( dirname( __FILE__ ) . "./../config/config.php" ) );
+    include_once( MODULES_PATH . "/user.php" );
+
+    if( isset( $_POST ) ) {
+        $user = new User();
+        $user->set( 'username', $_POST[ 'username' ] );
+        $user->set( 'password', $_POST[ 'password' ] );
+        $user_data_connector = new User_Data_Connector();
+        $user = $user_data_connector->login( $conn, $user );
+        if ( isset( $user ) ) 
+        {
+            $_SESSION[ 'user' ] = $user;
+            echo json_encode( array( "result" => true ) );
+        }
+        else
+        {
+            echo json_encode( array( "result" => false ) );
+        }
+    }
+
+?>
