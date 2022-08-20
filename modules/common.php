0995019c96643e66ec6e48ff443e966beb8f0099
Ng Qi Teng <Cmdcool3@gmail.com>
2022-08-21 02:18:17 +0800

    Create Common Class

---
+++ modules/common.php
@@ -0,0 +1,13 @@
+<?php
+
+    class Common
+    {
+
+        public static function convert_two_decimal( $value )
+        {
+            return sprintf( '%0.2f', round( $value, 2 ) );;
+        }
+
+    }
+
+?>
