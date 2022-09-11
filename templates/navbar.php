45d4cc667f3d96feee4f7f981aa7db4cf48f62fb
Ng Qi Teng <63818402+teng-1206@users.noreply.github.com>
2022-09-11 16:55:49 +0800

    Update Navbar
    Update dashboard link

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,18 +1,16 @@
 <!--  BEGIN NAVBAR  -->
 <div class="header-container fixed-top">
     <header class="header navbar navbar-expand-sm">
-
         <ul class="navbar-item theme-brand flex-row  text-center">
             <li class="nav-item theme-logo">
-                <a href="index.html">
+                <a href="dashboard">
                     <img src="<?= $config[ 'urls' ][ 'img' ] . 'icon-64x64.png' ?>" class="navbar-logo" alt="logo">
                 </a>
             </li>
             <li class="nav-item theme-text">
-                <a href="index.html" class="nav-link"> Settings </a>
+                <a href="dashboard" class="nav-link"> Settings </a>
             </li>
         </ul>
-
         <ul class="navbar-item flex-row ml-md-0 ml-auto">
             <!-- <li class="nav-item align-self-center search-animated">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search toggle-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
@@ -23,7 +21,6 @@
                 </form>
             </li> -->
         </ul>
-
         <ul class="navbar-item flex-row ml-md-auto">
 
             <!-- <li class="nav-item dropdown language-dropdown">
