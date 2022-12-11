6cbe06dab4c9752161cf762b2bf3f93eef3b5538
Ng Qi Teng <63818402+teng-1206@users.noreply.github.com>
2022-12-11 16:26:31 +0800

    Restructure JS & CSS

---
+++ assets/js/custom/login.js
@@ -0,0 +1,49 @@
+$( document ).ready( () => {
+    // Button Login Function
+$( '#btn-login' ).click( () => {
+    $( '#login-form' ).addClass( 'was-validated' );
+    const e_username = $( '#username' );
+    const e_password = $( '#password' );
+    const v_username = e_username.val() == '';
+    const v_password = e_password.val() == '';
+    const res = v_username || v_password;
+    const api_url = $( 'meta[ name="api-url" ]' ).attr( 'content' );
+    const login_url = `${ api_url }/login.php`;
+    if ( ! res ) {
+        var data = {
+            username: e_username.val(),
+            password: e_password.val(),
+        };
+        $.ajax( {
+            type: "POST",
+            url: login_url,
+            data: data,
+            success: ( res ) => {
+                var res = JSON.parse( res );
+                if( res.result == true ) {
+                    window.location.href= "dashboard";
+                } else {
+                    Toast.fire( {
+                        icon : 'error',
+                        title: 'Username or Password Wrong'
+                    } );
+                    e_password.val( '' );
+                }
+            },
+            error: () => {
+                Toast.fire( {
+                    icon : 'error',
+                    title: 'AJAX Error'
+                } );
+            }
+        } );
+    } else {
+        Toast.fire( {
+            icon : 'error',
+            title: 'Login Failed'
+        } );
+    }
+    
+} );
+
+} );
