64236ac108aefc49114fd83b34eaa20039c0182a
Ng Qi Teng <63818402+teng-1206@users.noreply.github.com>
2023-01-01 15:07:18 +0800

    Update footer.php
    1. Update Year

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -2,6 +2,6 @@
     <div class="footer-section f-section-1">
     </div>
     <div class="footer-section f-section-2">
-    <p class="">Copyright © 2022 All rights reserved</p>
+    <p class="">Copyright © 2022 - 2023 All rights reserved</p>
     </div>
 </div>
