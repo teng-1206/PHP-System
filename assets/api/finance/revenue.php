5854b4da366530220c2cf0232b864446af0967c3
Ng Qi Teng <63818402+teng-1206@users.noreply.github.com>
2023-01-14 18:26:45 +0800

    Update Dashboard
    1. Done income and expense dashboard.
    2. Current year revenue chart. Yearly & Weekly.

---
+++ assets/api/finance/revenue.php
@@ -0,0 +1,121 @@
+<?php
+    include_once( realpath( dirname( __FILE__ ) . "//..//..//config//config.php" ) );
+    include_once( MODULES_PATH . "finance.php" );
+    include_once( MODULES_PATH . "common.php" );
+
+    if ( isset( $_POST ) && isset( $_POST[ 'fk_user_id' ] ) && isset( $_POST[ 'choice' ] ) )
+    {
+        // Define user id & choice
+        $user_id = htmlspecialchars( $_POST[ 'fk_user_id' ] );
+        $choice = htmlspecialchars( $_POST[ 'choice' ] );
+
+        // Setup income, expense, label
+        $incomes = array();
+        $expenses = array();
+        $labels = array();
+
+        // Get all finance records
+        $finance = new Finance();
+        $finance->set( 'fk_user_id', $user_id );
+        $finance_controller = new Finance_Controller();
+        $all_finance = $finance_controller->read_all_by_user_id( $conn, $finance );
+        $all_finance = $crypto->decrypt_all_object( $all_finance );
+
+        // 
+        switch ( $choice ) 
+        {
+            case 'weekly':
+                $labels = array( 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun' );
+                $current_date = date( 'D' ) != 'Mon' ? date( 'Y-m-d', strtotime( 'last Monday' ) ) : date( 'Y-m-d' );
+                for ( $i = 1; $i < 8; $i++ ) 
+                {
+                    $total_income = 0;
+                    $total_expense = 0;
+                    foreach ( $all_finance as $finance )
+                    {
+                        $finance_current_date = date( 'Y-m-d', strtotime( $finance[ 'date' ] ) );
+                        if ( $finance_current_date == $current_date && $finance[ 'status' ] == false ) 
+                            $total_income += ( float ) $finance[ 'amount' ];
+                        if ( $finance_current_date == $current_date && $finance[ 'status' ] == true ) 
+                            $total_expense += ( float ) $finance[ 'amount' ];
+                    }
+                    $total_income  = Common::convert_two_decimal( $total_income );
+                    $total_expense = Common::convert_two_decimal( $total_expense );
+                    array_push( $incomes, $total_income );
+                    array_push( $expenses, $total_expense );
+                    $current_date = date( 'Y-m-d', strtotime( $current_date . ' +1 day' ) );
+                }
+                break;
+            case 'monthly':
+                $current_month = date( 'n' );
+                $current_year = date( 'Y' );
+                $days = cal_days_in_month( CAL_GREGORIAN, $current_month, $current_year );
+                for ( $i = 1; $i < $days + 1; $i++ ) 
+                {
+                    $current_date = date( 'Y-m-d', strtotime( $current_year . '-' . $current_month . '-' . $i ) );
+                    $total_income = 0;
+                    $total_expense = 0;
+                    foreach ( $all_finance as $finance )
+                    {
+                        $finance_current_date = date( 'Y-m-d', strtotime( $finance[ 'date' ] ) );
+                        if ( $finance_current_date == $current_date && $finance[ 'status' ] == false ) 
+                            $total_income += ( float ) $finance[ 'amount' ];
+                        if ( $finance_current_date == $current_date && $finance[ 'status' ] == true ) 
+                            $total_expense += ( float ) $finance[ 'amount' ];
+                    }
+                    $total_income  = Common::convert_two_decimal( $total_income );
+                    $total_expense = Common::convert_two_decimal( $total_expense );
+                    array_push( $incomes, $total_income );
+                    array_push( $expenses, $total_expense );
+                    array_push( $labels, $i );
+                }
+                break;
+            case 'yearly':
+                $labels = array( 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' );
+                $current_year = date( 'Y' );
+                for ( $i = 1; $i < 13; $i++ ) 
+                {
+                    $total_income = 0;
+                    $total_expense = 0;
+                    $m = ( strlen( ( string ) $i ) > 1 ) ? '0' . $i  : $i ;
+                    $year_month = ( $choice == 'yearly' ) ? date( 'Y-m', strtotime( date( 'Y-'. $m ) ) ) : date( 'Y-m', strtotime( date( 'Y-'. $m ) . ' -1 year') );
+                    
+                    foreach ( $all_finance as $finance )
+                    {
+                        $finance_year_current_month = date( 'Y-m', strtotime( $finance[ 'date' ] ) );
+                        if ( $finance_year_current_month == $year_month && $finance[ 'status' ] == false ) 
+                            $total_income += ( float ) $finance[ 'amount' ];
+
+                        if ( $finance_year_current_month == $year_month && $finance[ 'status' ] == true ) 
+                            $total_expense += ( float ) $finance[ 'amount' ];
+                    }
+                    $total_income  = Common::convert_two_decimal( $total_income );
+                    $total_expense = Common::convert_two_decimal( $total_expense );
+                    array_push( $incomes, $total_income );
+                    array_push( $expenses, $total_expense );
+                }
+                break;
+            default:
+                $labels = array( 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' );
+                break;
+        }
+        
+        if ( ! is_null( $all_finance ) )
+        {
+            
+            $res = array(
+                "result" => true,
+                "data" => array(
+                    "income" => $incomes,
+                    "expense" => $expenses,
+                    "label" => $labels
+                ),
+            );
+            echo json_encode( $res );
+        }
+        else 
+        {
+            echo json_encode( array( "result" => false ) );
+        }
+    }
+?>
--- public/dashboard.php
+++ public/dashboard.php
@@ -1,5 +1,12 @@
 <?php include_once( realpath( dirname( __FILE__ ) . "//assets//config//config.php" ) ); ?>
 <?php include_once( TEMPLATES_PATH . 'validation.php' ); ?>
+<?php include_once( MODULES_PATH . 'user.php' ); ?>
+<?php
+    $user_controller = new User_Controller();
+    $user = new User();
+    $user->set( 'id', $_SESSION[ 'user_id' ] );
+    $user = $user_controller->read( $conn2, $user );
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -42,6 +49,8 @@
     </div>
     <!-- Breadcrumb End -->
 
+    <input type="hidden" id="m-user-id" name="m-user-id" value="<?= $user->get( 'id' ) ?>">
+
     <!-- Main Container Start -->
     <div class="main-container" id="container">
 
@@ -55,29 +64,27 @@
         <!--  BEGIN CONTENT AREA  -->
         <div id="content" class="main-content">
             <div class="layout-px-spacing">
-
                 <div class="row layout-top-spacing">
-
                     <div class="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                         <div class="widget widget-chart-one">
                             <div class="widget-heading">
-                                <h5 class="">Revenue</h5>
+                                <h5 class="">2023 Revenue</h5>
                                 <div class="task-action">
                                     <div class="dropdown">
                                         <a class="dropdown-toggle" href="#" role="button" id="pendingTask" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
                                         </a>
                                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="pendingTask" style="will-change: transform;">
-                                            <a class="dropdown-item" href="javascript:void(0);">Weekly</a>
-                                            <a class="dropdown-item" href="javascript:void(0);">Monthly</a>
-                                            <a class="dropdown-item" href="javascript:void(0);">Yearly</a>
+                                            <a class="dropdown-item" href="javascript:void( load_revenue('weekly') );">Week</a>
+                                            <a class="dropdown-item" href="javascript:void( load_revenue('monthly') );">Month</a>
+                                            <a class="dropdown-item" href="javascript:void( load_revenue('yearly') );">Year</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
                             <div class="widget-content">
-                                <div id="revenueMonthly"></div>
+                                <div id="revenueMonthly">
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -206,8 +213,6 @@
                         </div>
                     </div>
 
-
-
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
                         <div class="widget-one widget">
                             <div class="widget-content">
@@ -226,7 +231,6 @@
                             </div>
                         </div>
                     </div>
-
 
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
 
@@ -410,7 +414,6 @@
                             </div>
                         </div>
                     </div>
-
 
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
                         <div class="widget widget-table-one">
@@ -618,10 +621,6 @@
                         </div>
                     </div>
 
-                    
-                    
-
-
                     <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                         <div class="widget widget-table-two">
 
@@ -691,7 +690,6 @@
                             </div>
                         </div>
                     </div>
-                    
 
                     <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                         <div class="widget widget-table-three">
@@ -756,9 +754,6 @@
                             </div>
                         </div>
                     </div>
-
-                    
-
                 </div>
 
             </div>
@@ -777,6 +772,277 @@
 
     <script src="<?= $config[ 'urls' ][ 'plugins' ] . "apex/apexcharts.min.js"; ?>"></script>
     <script src="<?= $config[ 'urls' ][ 'js' ] . "dashboard/dash_1.js"; ?>"></script>
+
+    <script>
+    /*
+        =================================
+            Revenue Monthly | Options
+        =================================
+    */
+    var chart;
+
+
+    function load_revenue( choice ) {
+        chart.destroy();
+        const revenue_url = `${ api_url }finance/revenue.php`;
+        const fk_user_id = $( '#m-user-id' ).val();
+        const sent_data = { fk_user_id, choice };
+
+        $.ajax( {
+            type    : 'POST',
+            url     : revenue_url,
+            data: sent_data,
+            dataType: 'JSON',
+            success: ( res ) => {
+                if ( res.result ) {
+                    console.log( res.data );
+                    display_revenue( res.data );
+                }
+                return res;
+            },
+            error: ( err ) => {
+                Toast.fire( {
+                    icon : 'error',
+                    title: 'Read Revenue Error'
+                } );
+            }
+        } );
+    }
+
+    function display_revenue( data ) {
+        const { income, expense, label } = data;
+        var option = {
+            chart: {
+                fontFamily: 'Nunito, sans-serif',
+                height: 365,
+                type: 'area',
+                zoom: {
+                    enabled: false
+                },
+                dropShadow: {
+                    enabled: true,
+                    opacity: 0.2,
+                    blur: 10,
+                    left: -7,
+                    top: 22
+                },
+                toolbar: {
+                    show: false
+                },
+                events: {
+                    mounted: function( ctx, config ) {
+                        const highest1 = ctx.getHighestValueInSeries(0);
+                        const highest2 = ctx.getHighestValueInSeries(1);
+                        ctx.addPointAnnotation({
+                            x: new Date(ctx.w.globals.seriesX[0][ctx.w.globals.series[0].indexOf(highest1)]).getTime(),
+                            y: highest1,
+                            label: {
+                            style: {
+                                cssClass: 'd-none'
+                            }
+                            },
+                            customSVG: {
+                                SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#2196f3" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
+                                cssClass: undefined,
+                                offsetX: -8,
+                                offsetY: 5
+                            }
+                        })
+                        ctx.addPointAnnotation({
+                            x: new Date(ctx.w.globals.seriesX[1][ctx.w.globals.series[1].indexOf(highest2)]).getTime(),
+                            y: highest2,
+                            label: {
+                            style: {
+                                cssClass: 'd-none'
+                            }
+                            },
+                            customSVG: {
+                                SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#e7515a" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
+                                cssClass: undefined,
+                                offsetX: -8,
+                                offsetY: 5
+                            }
+                        })
+                    },
+                }
+            },
+            colors: [ '#2196f3', '#e7515a' ],
+            dataLabels: {
+                enabled: false
+            },
+            markers: {
+                discrete: [{
+                seriesIndex: 0,
+                dataPointIndex: 7,
+                fillColor: '#000',
+                strokeColor: '#000',
+                size: 5
+                }, {
+                seriesIndex: 2,
+                dataPointIndex: 11,
+                fillColor: '#000',
+                strokeColor: '#000',
+                size: 4
+                }]
+            },
+            subtitle: {
+                text    : '0',
+                align   : 'left',
+                margin  : 0,
+                offsetX : 95,
+                offsetY : 0,
+                floating: false,
+                style   : {
+                    fontSize: '18px',
+                    color   : '#4361ee'
+                }
+            },
+            title: {
+                text: 'Total Earning',
+                align: 'left',
+                margin: 0,
+                offsetX: -10,
+                offsetY: 0,
+                floating: false,
+                style: {
+                    fontSize: '18px',
+                    color:  '#bfc9d4'
+                },
+            },
+            stroke: {
+                show: true,
+                curve: 'smooth',
+                width: 2,
+                lineCap: 'square'
+            },
+            series: [
+                {
+                    name: 'Income',
+                    data: income
+                },
+                {
+                    name: 'Expense',
+                    data: expense
+                }
+            ],
+            labels: label,
+            xaxis: {
+                axisBorder: {
+                    show: false
+                },
+                axisTicks: {
+                    show: false
+                },
+                crosshairs: {
+                    show: true
+                },
+                labels: {
+                    offsetX: 0,
+                    offsetY: 5,
+                    style: {
+                        fontSize: '12px',
+                        fontFamily: 'Nunito, sans-serif',
+                        cssClass: 'apexcharts-xaxis-title',
+                    },
+                }
+            },
+            yaxis: {
+                labels: {
+                    formatter: function(value, index) {
+                        // return (value / 1000) + 'K'
+                        return value
+                    },
+                    offsetX: -22,
+                    offsetY: 0,
+                    decimalsInFloat: 2,
+                    style: {
+                        fontSize: '12px',
+                        fontFamily: 'Nunito, sans-serif',
+                        cssClass: 'apexcharts-yaxis-title',
+                    },
+                }
+            },
+            grid: {
+                borderColor: '#191e3a',
+                strokeDashArray: 5,
+                xaxis: {
+                    lines: {
+                        show: true
+                    }
+                },   
+                yaxis: {
+                    lines: {
+                        show: false,
+                    }
+                },
+                padding: {
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: -10
+                }, 
+            }, 
+            legend: {
+                position: 'top',
+                horizontalAlign: 'right',
+                offsetY: -50,
+                fontSize: '16px',
+                fontFamily: 'Quicksand, sans-serif',
+                markers: {
+                    width: 10,
+                    height: 10,
+                    strokeWidth: 0,
+                    strokeColor: '#fff',
+                    fillColors: undefined,
+                    radius: 12,
+                    onClick: undefined,
+                    offsetX: 0,
+                    offsetY: 0
+                },    
+                itemMargin: {
+                    horizontal: 0,
+                    vertical: 20
+                }
+            },
+            tooltip: {
+                theme: 'dark',
+                marker: {
+                    show: true,
+                },
+                x: {
+                    show: false,
+                }
+            },
+            fill: {
+                type:"gradient",
+                gradient: {
+                    type: "vertical",
+                    shadeIntensity: 1,
+                    inverseColors: !1,
+                    opacityFrom: .19,
+                    opacityTo: .05,
+                    stops: [100, 100]
+                }
+            },
+            responsive: [{
+                breakpoint: 575,
+                options: {
+                    legend: {
+                        offsetY: -30,
+                    },
+                },
+            }]
+        }
+
+        chart = new ApexCharts(
+            document.querySelector( "#revenueMonthly" ),
+            option
+        );
+        chart.render();
+    }
+
+    load_revenue( 'weekly' );
+    </script>
 
 </body>
 </html>
