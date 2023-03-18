62a5912a2c3011c693cb922be0e51425a643a1eb
Ng Qi Teng <63818402+teng-1206@users.noreply.github.com>
2023-03-18 19:48:08 +0800

    Update create.php
    Add fk_user_id

--- assets/api/poop/create.php
+++ assets/api/poop/create.php
@@ -2,10 +2,11 @@
     include_once( realpath( dirname( __FILE__ ) . "//..//..//config//config.php" ) );
     include_once( MODULES_PATH . "poop.php" );
 
-    if ( isset( $_POST ) && isset( $_POST['date'] ) )
+    if ( isset( $_POST ) && isset( $_POST['date'] ) && isset( $_POST['fk_user_id'] ) )
     {
         $poop = new Poop();
         $poop->set( 'date', htmlspecialchars( $_POST[ 'date' ] ) );
+        $poop->set( 'fk_user_id', htmlspecialchars( $_POST[ 'fk_user_id' ] ) );
 
         $poop_controller = new Poop_Controller();
         $poop_id = $poop_controller->create( $conn, $poop );
